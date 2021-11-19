c3452819fb9d03386c6dad8b072b81168068b9f3
Sergio Asenjo <sergio.asenjo@usach.cl>
2021-11-19 13:21:46 -0300

    update db conn

--- src/db.py
+++ src/db.py
@@ -7,7 +7,7 @@ DRIVER = "ODBC Driver 17 for SQL Server"
 USERNAME = os.environ["DB_USER"]
 PASSWORD = os.environ["DB_PASS"]
 INSTANCE_NAME = os.environ["DB_INSTANCE"]
-DATABASE_CONNECTION = f"mssql://{USERNAME}:{PASSWORD}@{SERVER}\{INSTANCE_NAME},1433/{DATABASE}?driver={DRIVER}"
+DATABASE_CONNECTION = f"mssql://{USERNAME}:{PASSWORD}@{SERVER}/{DATABASE}?driver={DRIVER}"
 
 engine = create_engine(DATABASE_CONNECTION)
 meta = MetaData()
